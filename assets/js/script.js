a7568b9e3c1b2b535e2e732ee668a6c5ac0b5885
unknown <jean-bateee@hotmail.fr>
2021-05-17 14:51:50 +0200

    add cross

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,6 +57,7 @@ function timer() {
     timerFunction = setInterval(function () {
         timerNum--
         cross()
+        crossRight()
         $("#timer").html("Timer : " + timerNum)
         if (timerNum == 0) {
             console.log('fini')
@@ -66,6 +67,50 @@ function timer() {
         }
     },1000)
 }
+
+function cross() {
+    for (let i = 0; i < 90; i += 15) {
+        if (timerNum <= 75 && timerNum == i) {
+            $("#crossScreen").show()
+            let randomCross = Math.floor(Math.random() * 80) + 10;
+            $("#crossScreen").css("top", randomCross + '%')
+            $("#crossScreen").addClass("IG_bonustarget");
+            setTimeout(function () {
+                $("#crossScreen").removeClass("IG_bonustarget");
+            }, 3000)
+        }
+    }
+}
+
+
+function crossRight() {
+    for (let i = 0; i < 90; i += 20) {
+        if (timerNum <= 75 && timerNum == i) {
+            $("#crossScreenRight").show()
+            let randomCross = Math.floor(Math.random() * 80) + 10;
+            $("#crossScreenRight").css("top", randomCross + '%')
+            $("#crossScreenRight").addClass("IG_bonustargetRight");
+            setTimeout(function () {
+                $("#crossScreenRight").removeClass("IG_bonustargetRight");
+            }, 3000)
+        }
+    }
+}
+
+
+
+$("#crossScreen").click(function () {
+    score = score + 3
+    $("#crossScreen").hide()
+
+})
+
+$("#crossScreenRight").click(function () {
+    score = score + 4
+    $("#crossScreenRight").hide()
+
+})
+
 
 
 
