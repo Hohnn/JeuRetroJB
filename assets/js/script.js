f209f6821857f0477506d339fa943dba80e23eca
unknown <jean-bateee@hotmail.fr>
2021-05-17 12:07:44 +0200

    v1

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+/* -----------------------------------------------
+    Fichier JS
+----------------------------------------------- */
+
+// Vérification de la bonne connexion
+console.log("Connexion JS - Ok")
+
+//animation target
+$("#target").hide()
+const target = document.getElementById("target")
+const button = document.getElementById("button")
+let timerNum = 90
+$("button").click( function () {
+    start()
+    interval()
+    timer()
+    timerNum = 90
+    
+}
+    
+)
+function start (){
+    let randomNumber = Math.floor(Math.random() * (80 +10));
+    let randomNumber2 = Math.floor(Math.random() * (80+10));
+    $("#target").css("top", randomNumber + '%')
+    $("#target").css("left", randomNumber2 + '%')
+    $("#target").show()
+   
+}
+
+let score = 0
+
+$("#target").click(function(){
+    $("#target").hide()
+    score++
+    $("#score").html("Score : " + score)
+    timePaste = 0
+})
+
+let timePaste = 0
+let targetInterval
+function interval() {
+    console.log('interval')
+    targetInterval = setInterval(function () {
+        console.log('timeout')
+        timePaste++
+        console.log(timePaste)
+        if (timePaste == 3) {
+            start()
+            timePaste = 0
+        }
+    }, 1000)
+}
+
+
+
+function timer() {
+    let timerFunction = setInterval(function () {
+        timerNum--
+        $("#timer").html("Timer : " + timerNum)
+        if (timerNum == 0) {
+            console.log('fini')
+            
+            clearInterval(timerFunction);
+            clearInterval(targetInterval);
+            $("#target").hide()
+        }
+    },1000)
+}
+
